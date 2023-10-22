8531b9fc62c5c0e5b0c7aa9ff25815b15a05c050
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-10-22 23:22:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+// Create 800 squares dynamically
+const squareContainer = document.querySelector('.square-container');
+
+for (let i = 0; i < 800; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    squareContainer.appendChild(square);
+}
+
+// Reset square colors after 1 second
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+        setTimeout(() => {
+            square.style.backgroundColor = '#ccc';
+        }, 1000);
+    });
+});
